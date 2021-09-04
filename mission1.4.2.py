7c202b99fa90a7ccc9be5a05987ba8fe1743f8fb
ron harel <rontsohrl@gmail.com>
2021-09-04 21:17:26 +0300

    Initial commit

---
+++ mission1.4.2.py
@@ -0,0 +1,60 @@
+print("picture 1")
+print("x-------x")
+
+print("picture 2")
+print("""x-------x
+|
+|
+|
+|
+|
+""")
+
+print("picture 3")
+
+print("""x-------x
+|       |
+|       0
+|
+|
+|
+""")
+
+print("picture 4")
+
+print("""x-------x
+|       |
+|       0
+|       |
+|
+|
+""")
+
+print("picture 5")
+
+print("""x-------x
+|       |
+|       0
+|     / | /
+|       
+|
+""")
+print("picture 6")
+
+print("""x-------x
+|       |
+|       0
+|     / | /
+|     /   
+|
+""")
+
+print("picture 7")
+
+print("""x-------x
+|       |
+|       0
+|     / | /
+|     /   /
+|
+""")
